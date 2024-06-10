c71729f713da64088b2e500a65d1b4215d6cc900
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-06-10 10:49:24 +0530

    11b array index

--- exercise-solutions/lesson-11/11a.js
+++ exercise-solutions/lesson-11/11a.js
@@ -1,3 +1,6 @@
-const nums = [10,20, 30];
-nums[2] = 99;
-console.log(nums);
+function getLastValue(array) {
+  const lastIndex = array.length - 1;
+  return array[lastIndex];
+}
+console.log(getLastValue([1, 20, 22, 24, 5]));
+console.log(getLastValue(['hi', 'hello', 'hey']));
