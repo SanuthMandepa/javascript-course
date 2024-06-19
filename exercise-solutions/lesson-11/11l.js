a84bb09e9049ec67385dba9fd131042a4e2c4360
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-06-19 08:53:47 +0530

    11l min max

---
+++ exercise-solutions/lesson-11/11l.js
@@ -0,0 +1,21 @@
+function minMax(nums){
+  let min = null;
+  let max = null;
+
+  for(let i = 0; i< nums.length;i++){
+    const value = nums[i];
+    if (value ===null || value < min){
+      min = value;
+    }
+
+    if(value === max || value > max){
+      max = value;
+    }
+  }
+  return { min, max };
+}
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, 10]));
+
+console.log(minMax([]));
+console.log(minMax([3]));
